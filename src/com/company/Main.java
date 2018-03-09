9bb8555418168ac3aea408bfb6a7af2f97e18609
Dawa Sherpa <dasherpa7@gmail.com>
2018-03-09 15:38:47 -0500

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("----------------------------");
+        System.out.println("Welcome to this typing challenge!");
+        System.out.println("Enter your name!");
+        System.out.println("----------------------------");
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        System.out.println("Enter a number: ");
+        int n = reader.nextInt(); // Scans the next token of the input as an int.
+//once finished
+        reader.close();
+        System.out.println("You entered " + n);
+    }
+}
