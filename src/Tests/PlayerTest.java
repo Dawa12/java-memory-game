5e28bf1aca13562ad786a3cefcf669d7789d2bd7
Dawa Sherpa <dasherpa7@gmail.com>
2018-03-10 10:46:43 -0500

    passing first test

--- src/Tests/PlayerTest.java
+++ src/Tests/PlayerTest.java
@@ -1,4 +1,18 @@
 package Tests;
+import com.company.Player;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
 public class PlayerTest {
+    @Test
+    public void testPlayerInputLength() {
+        Player bob = new Player("Bob");
+        bob.setName("bob");
+//        check if player entered valid name
+        assertTrue(bob.getName().length() > 1);
+    }
+
+
+
+
 }
