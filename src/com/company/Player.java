b7efc85ad04dd90b35e5ada59842d3a822e58d18
Dawa Sherpa <dasherpa7@gmail.com>
2018-03-10 10:47:33 -0500

    create class with getters and setters

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,4 +1,20 @@
 package com.company;
 
 public class Player {
+    private String name;
+
+    public Player(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+//        check against null and empty name
+        if(name != null && !name.trim().isEmpty()) {
+            this.name = name;
+        }
+    }
 }
