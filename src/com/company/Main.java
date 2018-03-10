3f9907b03a7dab18275b73fa1528770865d87d72
Dawa Sherpa <dasherpa7@gmail.com>
2018-03-10 12:21:42 -0500

    add getRandomString() and calculateScore methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -7,11 +9,49 @@ public class Main {
         System.out.println("Welcome to the Typing Challenge!");
         System.out.println("Enter your name!");
         System.out.println("----------------------------");
-        Scanner reader = new Scanner(System.in);  // Reading from System.in
-        System.out.println("Enter a number: ");
-        int n = reader.nextInt(); // Scans the next token of the input as an int.
-//once finished
+
+//        Scanner reader = new Scanner(System.in);  // Reading from System.in
+//        System.out.println("Enter your Name: ");
+//        String n = reader.nextLine(); // Scans the next token of the input as an String.
+////once finished
+//        System.out.println("Welcome " + n + "!");
+
+        String randomString = getRandomString();
+        System.out.println("Type along!");
+        System.out.println(randomString);
+
+        Scanner reader = new Scanner(System.in);
+        String n = reader.nextLine();
+
+        calculateScore(randomString, n);
         reader.close();
-        System.out.println("You entered " + n);
+    }
+
+    public static String getRandomString() {
+//        String pattern = "(.*)(\\d+)(.*)";
+        String randomString = new String("abcdefg");
+        return randomString;
+    }
+
+    public static String calculateScore(String randomString,String userInput) {
+        int score = 0;
+//    increment score for every character that matches userInput
+        for (int i = 0; i < randomString.length(); i++) {
+            try {
+                userInput.charAt( i );
+            } catch (Exception e){
+//                try to catch OutofBoundsException
+                System.out.println(e);
+                break;
+            }
+
+            if(randomString.charAt( i ) == userInput.charAt( i
+            )) {
+               score++;
+            }
+//            System.out.println(userInput.charAt(i));
+        }
+        System.out.println("Score: " + score);
+        return "h";
     }
 }
